6e68dc62c5fa30798a5f0400655e3add26ff7969
abida-h <abida@hauser.ca>
2026-02-25 15:04:55 -0500

    Trigger rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/my-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
