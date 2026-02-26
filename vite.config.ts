afeb49c001212b482aa016eadd020fd40515c22d
abida-h <abida@hauser.ca>
2026-02-26 10:24:29 -0500

    Changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/abidhauser.github.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
